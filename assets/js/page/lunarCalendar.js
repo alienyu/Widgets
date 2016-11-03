dcc0939837bb484b0bb09da312f1cbec0f474181
alanyu <yuyongjian@didichuxing.com>
2016-11-03 18:41:34 +0800

    Dg

--- assets/js/page/lunarCalendar.js
+++ assets/js/page/lunarCalendar.js
@@ -6,6 +6,7 @@ var fuc = {
     config: {},
     init: function() {
         var that = this;
+        $(".calendar .time").text(new Date().getHours() + ":" + new Date().getMinutes());
         this.lunar = new LunarCalendar({
             initOver: function(date) {
                 that.config.currentDate = date;
@@ -28,7 +29,16 @@ var fuc = {
                 that.switchCalendarText(date);
             }
         });
-        this.timeSelect = new timeSelect();
+        this.timeSelect = new timeSelect({
+            dom: "timeSelect",
+            hour: new Date().getHours(),
+            minute: new Date().getMinutes(),
+            changeTimeCallback: function(hour, min) {
+                hour = (hour.toString().length == 1) ? ("0" + hour.toString()) : hour;
+                min = (min.toString().length == 1) ? ("0" + min.toString()) : min;
+                $(".calendar .time").text(hour + ":" + min);
+            }
+        });
         this.bindEvent();
     },
     switchCalendarText: function(date) {
@@ -45,12 +55,16 @@ var fuc = {
         this.addCalendarTextHiddenDate(date.cYear, date.cMonth, date.cDay);;
     },
     addCalendarTextHiddenDate: function(year, month ,day) {
-        $(".calendar .date").attr("date-year", year).attr("date-month", month).attr("date-day", day);
+        $(".calendar .date").attr("data-year", year).attr("data-month", month).attr("data-day", day);
     },
     bindEvent: function() {
         var that = this;
         $('.lunar input').change(function(e) {
             var calendarText = "";
+            $(".date").addClass("active");
+            $(".time").removeClass("active");
+            $("#calendar").removeClass("hide");
+            $("#timeSelect").addClass("hide");
             if(e.target.checked) {
                 that.lunar.switchMode("lunar");
                 calendarText = that.config.currentDate.cYear + "年" + that.config.currentDate.IMonthCn + that.config.currentDate.IDayCn;
@@ -63,6 +77,32 @@ var fuc = {
         });
         $("#toToday").click(function(e) {
             that.lunar.toToday();
+        });
+
+        $(".full_day input").change(function(e) {
+           if(e.target.checked) {
+               that.timeSelect.changeHM(0,0);
+               $(".calendar .time").text("00:00");
+           }
+        });
+
+        $('.calendar').delegate("div","tap", function(e) {
+            if($(e.target).hasClass('date')) {
+                $(".date").addClass("active");
+                $(".time").removeClass("active");
+                $("#calendar").removeClass("hide");
+                $("#timeSelect").addClass("hide");
+            } else {
+                $(".date").removeClass("active");
+                $(".time").addClass("active");
+                $("#calendar").addClass("hide");
+                $("#timeSelect").removeClass("hide");
+                that.timeSelect.changeHM(parseInt($(".calendar .time").text().split(":")[0], 10), parseInt($(".calendar .time").text().split(":")[1], 10))
+            }
+        });
+
+        $(".confirm").bind("tap", function(e) {
+            alert($(".calendar .date").data("year") + "-" + $(".calendar .date").data("month") + "-" + $(".calendar .date").data("day") + " " + $(".calendar .time").text());
         });
     }
 }
